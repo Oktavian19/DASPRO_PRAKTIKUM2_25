83736fd7423f7bfe4ec6667aa335802988e84970
Oktavian19 <oktavianeka4@gmail.com>
2023-09-08 14:31:51 +0700

    simpan kode ke java 25

---
+++ MyFirstJavaGithub25.java
@@ -0,0 +1,7 @@
+package MINGGU_2.DASPRO_PRAKTIKUM2_25;
+
+public class MyFirstJavaGithub25 {
+    public static void main(String[] args) {
+        System.out.println("Hello from Github");
+    } 
+}
