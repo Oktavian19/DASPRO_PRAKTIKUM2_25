9b6683a204abc49914c3cab8afc4c120b7c62661
Oktavian19 <oktavianeka4@gmail.com>
2023-09-08 14:57:31 +0700

    commit lagii

--- MyFirstJavaGithub25.java
+++ MyFirstJavaGithub25.java
@@ -4,5 +4,6 @@ public class MyFirstJavaGithub25 {
     public static void main(String[] args) {
         System.out.println("Hello from Github");
         System.out.println("Saya Nomor Absen : 25");  
+        System.out.println("blabla");
     } 
 }
